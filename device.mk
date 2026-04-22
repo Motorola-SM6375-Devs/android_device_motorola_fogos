d678abb67826d2832df39ae23a72ac8dbe4a6809
Anand S <anandzzz360@gmail.com>
2026-04-22 22:29:48 +0530

    fogos: Enable Lineage Health Fast charge control
    Change-Id: Ibd9709afd3ee146a011dabe56abb10a42efe4240

--- device.mk
+++ device.mk
@@ -56,6 +56,11 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 $(call soong_config_set_bool,livedisplay_sysfs,enable_ab,true)
 
+# Lineage Health
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/qcom-battery/restrict_chg)
+$(call soong_config_set,lineage_health,fast_charge_value_none,1)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.sec \
