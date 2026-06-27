e12e9ce171dd2bfe68d131d0092806c6ed307d6a
Anand S <anandzzz360@gmail.com>
2026-06-27 20:09:33 +0530

    fogos: Decommonise charging control path
    Change-Id: I2b632cf7fe8554c685ad96a95c71c179b9bd24df

--- device.mk
+++ device.mk
@@ -57,6 +57,10 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set_bool,livedisplay_sysfs,enable_ab,true)
 
 # Lineage Health
+$(call soong_config_set,lineage_health,charging_control_charging_path,sys/class/power_supply/battery/input_current_limit)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 $(call soong_config_set,lineage_health,fast_charge_node,/sys/class/qcom-battery/restrict_chg)
 $(call soong_config_set,lineage_health,fast_charge_value_none,1)
 $(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
